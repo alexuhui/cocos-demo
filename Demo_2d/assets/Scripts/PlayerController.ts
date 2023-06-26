6f152c3165011aa2280d92c59d4d2ff5d1133788
chenyouhui1 <chenyouhui1@joyy.com>
2023-06-26 17:39:37 +0800

    demo

--- Demo_2d/assets/Scripts/PlayerController.ts
+++ Demo_2d/assets/Scripts/PlayerController.ts
@@ -16,13 +16,22 @@ export class PlayerController extends Component {
     private _curJumpSpeed: number = 0;
     private _curPos: Vec3 = new Vec3();
     private _deltaPos: Vec3 = new Vec3(0, 0, 0);
-    private _targetPos: Vec3 = new Vec3();
-
+    private _targetPos: Vec3 = new Vec3();   
+    private _curMoveIndex: number = 0;
     start () {
-        input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+        //input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+    }
+
+    setInputActive(active: boolean) {
+        if (active) {
+            input.on(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+        } else {
+            input.off(Input.EventType.MOUSE_UP, this.onMouseUp, this);
+        }
     }
 
     reset() {
+        this._curMoveIndex = 0;
     }   
 
     onMouseUp(event: EventMouse) {
@@ -31,24 +40,32 @@ export class PlayerController extends Component {
         } else if (event.getButton() === 2) {
             this.jumpByStep(2);
         }
+
     }
 
     jumpByStep(step: number) {
         if (this._startJump) {
             return;
         }
+        this._startJump = true
+        this._jumpStep = step
+        this._curJumpTime = 0
 
         const clipName = step == 1 ? 'oneStep' : 'twoStep';
         const state = this.BodyAnim.getState(clipName);
         this._jumpTime = state.duration;
-        this.BodyAnim.play(clipName);
+        this.BodyAnim.play(clipName)
 
-        this._startJump = true;
-        this._jumpStep = step;
-        this._curJumpTime = 0;
         this._curJumpSpeed = this._jumpStep * BLOCK_SIZE/ this._jumpTime;
         this.node.getPosition(this._curPos);
         Vec3.add(this._targetPos, this._curPos, new Vec3(this._jumpStep* BLOCK_SIZE, 0, 0));  
+
+        this._curMoveIndex += step;
+    }
+
+
+    onOnceJumpEnd() {
+        this.node.emit('JumpEnd', this._curMoveIndex);
     }
 
     update (deltaTime: number) {
@@ -57,7 +74,8 @@ export class PlayerController extends Component {
             if (this._curJumpTime > this._jumpTime) {
                 // end
                 this.node.setPosition(this._targetPos);
-                this._startJump = false;              
+                this._startJump = false;   
+                this.onOnceJumpEnd();           
             } else {
                 // tween
                 this.node.getPosition(this._curPos);
